b09f48d751d8f96babf3a57bccd1d9446cdc81b6
Vikash Thakur <thakvikash622@gmail.com>
2024-04-09 13:18:18 +0530

    Provide react-router functionality

--- YoutTubeClone/src/App.jsx
+++ YoutTubeClone/src/App.jsx
@@ -1,10 +1,18 @@
 import Navbar from "./Components/Navbar/Navbar";
+import { Routes, Route } from 'react-router-dom'
+// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from '../src/Pages/Home/Home'
+import Video from '../src/Pages/Video/Video'
 
 
 const App = () => {
   return (
     <>
     <Navbar />
+    <Routes>
+      <Route path='/' element={<Home/>} ></Route>
+      <Route path='/video/:categoryId/:videoId' element={<Video/>} ></Route>
+    </Routes>
     </>
   );
 };
