990c46ff31ef7ebc72696772b377f2f05a671c1c
Vikash Thakur <thakvikash622@gmail.com>
2024-04-10 22:25:46 +0530

    Working on feed section

---
+++ YoutTubeClone/src/Components/Feed/Feed.jsx
@@ -0,0 +1,67 @@
+import './Feed.css'
+import { asset } from '../../assets/asset'
+
+const Feed = () => {
+  return (
+    <div className="feed">
+        <div className="card">
+            <img src={asset.thumbnail1} alt="" />
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum sunt eos natus iusto fuga dolorem eum magni nesciunt magnam reiciendis ad cumque sit alias ab rerum, voluptates cum consequatur soluta!
+            Facilis rem quae, enim tempora praesentium distinctio aperiam tenetur nihil iste odio maxime unde vel, quibusdam earum pariatur porro. Quia nemo aperiam nesciunt minima repellat voluptatibus tempore magni, id iusto.</p>
+            <h3>Vikash</h3>
+            <p>15k views &bull; 2 days ago</p>
+        </div>
+        <div className="card">
+            <img src={asset.thumbnail2} alt="" />
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum sunt eos natus iusto fuga dolorem eum magni nesciunt magnam reiciendis ad cumque sit alias ab rerum, voluptates cum consequatur soluta!
+            Facilis rem quae, enim tempora praesentium distinctio aperiam tenetur nihil iste odio maxime unde vel, quibusdam earum pariatur porro. Quia nemo aperiam nesciunt minima repellat voluptatibus tempore magni, id iusto.</p>
+            <h3>Vikash</h3>
+            <p>15k views &bull; 2 days ago</p>
+        </div>
+        <div className="card">
+            <img src={asset.thumbnail3} alt="" />
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum sunt eos natus iusto fuga dolorem eum magni nesciunt magnam reiciendis ad cumque sit alias ab rerum, voluptates cum consequatur soluta!
+            Facilis rem quae, enim tempora praesentium distinctio aperiam tenetur nihil iste odio maxime unde vel, quibusdam earum pariatur porro. Quia nemo aperiam nesciunt minima repellat voluptatibus tempore magni, id iusto.</p>
+            <h3>Vikash</h3>
+            <p>15k views &bull; 2 days ago</p>
+        </div>
+        <div className="card">
+            <img src={asset.thumbnail4} alt="" />
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum sunt eos natus iusto fuga dolorem eum magni nesciunt magnam reiciendis ad cumque sit alias ab rerum, voluptates cum consequatur soluta!
+            Facilis rem quae, enim tempora praesentium distinctio aperiam tenetur nihil iste odio maxime unde vel, quibusdam earum pariatur porro. Quia nemo aperiam nesciunt minima repellat voluptatibus tempore magni, id iusto.</p>
+            <h3>Vikash</h3>
+            <p>15k views &bull; 2 days ago</p>
+        </div>
+        <div className="card">
+            <img src={asset.thumbnail5} alt="" />
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum sunt eos natus iusto fuga dolorem eum magni nesciunt magnam reiciendis ad cumque sit alias ab rerum, voluptates cum consequatur soluta!
+            Facilis rem quae, enim tempora praesentium distinctio aperiam tenetur nihil iste odio maxime unde vel, quibusdam earum pariatur porro. Quia nemo aperiam nesciunt minima repellat voluptatibus tempore magni, id iusto.</p>
+            <h3>Vikash</h3>
+            <p>15k views &bull; 2 days ago</p>
+        </div>
+        <div className="card">
+            <img src={asset.thumbnail6} alt="" />
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum sunt eos natus iusto fuga dolorem eum magni nesciunt magnam reiciendis ad cumque sit alias ab rerum, voluptates cum consequatur soluta!
+            Facilis rem quae, enim tempora praesentium distinctio aperiam tenetur nihil iste odio maxime unde vel, quibusdam earum pariatur porro. Quia nemo aperiam nesciunt minima repellat voluptatibus tempore magni, id iusto.</p>
+            <h3>Vikash</h3>
+            <p>15k views &bull; 2 days ago</p>
+        </div>
+        <div className="card">
+            <img src={asset.thumbnail7} alt="" />
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum sunt eos natus iusto fuga dolorem eum magni nesciunt magnam reiciendis ad cumque sit alias ab rerum, voluptates cum consequatur soluta!
+            Facilis rem quae, enim tempora praesentium distinctio aperiam tenetur nihil iste odio maxime unde vel, quibusdam earum pariatur porro. Quia nemo aperiam nesciunt minima repellat voluptatibus tempore magni, id iusto.</p>
+            <h3>Vikash</h3>
+            <p>15k views &bull; 2 days ago</p>
+        </div>
+        <div className="card">
+            <img src={asset.thumbnail8} alt="" />
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum sunt eos natus iusto fuga dolorem eum magni nesciunt magnam reiciendis ad cumque sit alias ab rerum, voluptates cum consequatur soluta!
+            Facilis rem quae, enim tempora praesentium distinctio aperiam tenetur nihil iste odio maxime unde vel, quibusdam earum pariatur porro. Quia nemo aperiam nesciunt minima repellat voluptatibus tempore magni, id iusto.</p>
+            <h3>Vikash</h3>
+            <p>15k views &bull; 2 days ago</p>
+        </div>
+    </div>
+  );
+};
+
+export default Feed;
--- YoutTubeClone/src/Pages/Home/Home.jsx
+++ YoutTubeClone/src/Pages/Home/Home.jsx
@@ -1,11 +1,15 @@
+import Feed from '../../Components/Feed/Feed';
 import Sidebar from '../../Components/Sidebar/Sidebar';
 import './Home.css'
 
 const Home = ({sidebar}) => {
   return (
-    <>
+    <div className='main'>
     <Sidebar sidebar={sidebar} />
-    </>
+    {/* <div className={`container ${sidebar?"": "large-container"}`}>
+    </div> */}
+      <Feed />
+    </div>
   );
 };
 
