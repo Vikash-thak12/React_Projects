f51493a91494d9a8729fa9b0af408aa9b910ebf2
Vikash Thakur <thakvikash622@gmail.com>
2024-04-11 14:40:39 +0530

    Working on linking videos

--- YoutTubeClone/src/Components/Feed/Feed.jsx
+++ YoutTubeClone/src/Components/Feed/Feed.jsx
@@ -1,15 +1,16 @@
 import './Feed.css'
 import { asset } from '../../assets/asset'
+import { Link } from 'react-router-dom';
 
 const Feed = () => {
   return (
     <div className="feed">
-        <div className="card">
+        <Link to={`video/20/6834`} className="card">
             <img src={asset.thumbnail1} alt="" />
             <h2>The tranquil lake shimmered under the golden sunset, while birdsong filled the air, creating a symphony of natural beauty.</h2>
             <h3>Vikash</h3>
             <p>15k views &bull; 2 days ago</p>
-        </div>
+        </Link>
         <div className="card">
             <img src={asset.thumbnail2} alt="" />
             <h2>The tranquil lake shimmered under the golden sunset, while birdsong filled the air, creating a symphony of natural beauty.</h2>
