67234e4e243a26eb5e3efb9cc4e2390255c1d4b1
Vikash Thakur <thakvikash622@gmail.com>
2024-04-11 14:32:18 +0530

    Almost done with the feed section

--- YoutTubeClone/src/Components/Feed/Feed.jsx
+++ YoutTubeClone/src/Components/Feed/Feed.jsx
@@ -6,49 +6,97 @@ const Feed = () => {
     <div className="feed">
         <div className="card">
             <img src={asset.thumbnail1} alt="" />
-            <p>The tranquil lake shimmered under the golden sunset, while birdsong filled the air, creating a symphony of natural beauty.</p>
+            <h2>The tranquil lake shimmered under the golden sunset, while birdsong filled the air, creating a symphony of natural beauty.</h2>
             <h3>Vikash</h3>
             <p>15k views &bull; 2 days ago</p>
         </div>
         <div className="card">
             <img src={asset.thumbnail2} alt="" />
-            <p>The tranquil lake shimmered under the golden sunset, while birdsong filled the air, creating a symphony of natural beauty.</p>
+            <h2>The tranquil lake shimmered under the golden sunset, while birdsong filled the air, creating a symphony of natural beauty.</h2>
             <h3>Vikash</h3>
             <p>15k views &bull; 2 days ago</p>
         </div>
         <div className="card">
             <img src={asset.thumbnail3} alt="" />
-            <p>The tranquil lake shimmered under the golden sunset, while birdsong filled the air, creating a symphony of natural beauty.</p>
+            <h2>The tranquil lake shimmered under the golden sunset, while birdsong filled the air, creating a symphony of natural beauty.</h2>
             <h3>Vikash</h3>
             <p>15k views &bull; 2 days ago</p>
         </div>
         <div className="card">
             <img src={asset.thumbnail4} alt="" />
-            <p>The tranquil lake shimmered under the golden sunset, while birdsong filled the air, creating a symphony of natural beauty.</p>
+            <h2>The tranquil lake shimmered under the golden sunset, while birdsong filled the air, creating a symphony of natural beauty.</h2>
             <h3>Vikash</h3>
             <p>15k views &bull; 2 days ago</p>
         </div>
         <div className="card">
             <img src={asset.thumbnail5} alt="" />
-            <p>The tranquil lake shimmered under the golden sunset, while birdsong filled the air, creating a symphony of natural beauty.</p>
+            <h2>The tranquil lake shimmered under the golden sunset, while birdsong filled the air, creating a symphony of natural beauty.</h2>
             <h3>Vikash</h3>
             <p>15k views &bull; 2 days ago</p>
         </div>
         <div className="card">
             <img src={asset.thumbnail6} alt="" />
-            <p>The tranquil lake shimmered under the golden sunset, while birdsong filled the air, creating a symphony of natural beauty.</p>
+            <h2>The tranquil lake shimmered under the golden sunset, while birdsong filled the air, creating a symphony of natural beauty.</h2>
             <h3>Vikash</h3>
             <p>15k views &bull; 2 days ago</p>
         </div>
         <div className="card">
             <img src={asset.thumbnail7} alt="" />
-            <p>The tranquil lake shimmered under the golden sunset, while birdsong filled the air, creating a symphony of natural beauty.</p>
+            <h2>The tranquil lake shimmered under the golden sunset, while birdsong filled the air, creating a symphony of natural beauty.</h2>
             <h3>Vikash</h3>
             <p>15k views &bull; 2 days ago</p>
         </div>
         <div className="card">
             <img src={asset.thumbnail8} alt="" />
-            <p>The tranquil lake shimmered under the golden sunset, while birdsong filled the air, creating a symphony of natural beauty.</p>
+            <h2>The tranquil lake shimmered under the golden sunset, while birdsong filled the air, creating a symphony of natural beauty.</h2>
+            <h3>Vikash</h3>
+            <p>15k views &bull; 2 days ago</p>
+        </div>
+        <div className="card">
+            <img src={asset.thumbnail1} alt="" />
+            <h2>The tranquil lake shimmered under the golden sunset, while birdsong filled the air, creating a symphony of natural beauty.</h2>
+            <h3>Vikash</h3>
+            <p>15k views &bull; 2 days ago</p>
+        </div>
+        <div className="card">
+            <img src={asset.thumbnail2} alt="" />
+            <h2>The tranquil lake shimmered under the golden sunset, while birdsong filled the air, creating a symphony of natural beauty.</h2>
+            <h3>Vikash</h3>
+            <p>15k views &bull; 2 days ago</p>
+        </div>
+        <div className="card">
+            <img src={asset.thumbnail3} alt="" />
+            <h2>The tranquil lake shimmered under the golden sunset, while birdsong filled the air, creating a symphony of natural beauty.</h2>
+            <h3>Vikash</h3>
+            <p>15k views &bull; 2 days ago</p>
+        </div>
+        <div className="card">
+            <img src={asset.thumbnail4} alt="" />
+            <h2>The tranquil lake shimmered under the golden sunset, while birdsong filled the air, creating a symphony of natural beauty.</h2>
+            <h3>Vikash</h3>
+            <p>15k views &bull; 2 days ago</p>
+        </div>
+        <div className="card">
+            <img src={asset.thumbnail5} alt="" />
+            <h2>The tranquil lake shimmered under the golden sunset, while birdsong filled the air, creating a symphony of natural beauty.</h2>
+            <h3>Vikash</h3>
+            <p>15k views &bull; 2 days ago</p>
+        </div>
+        <div className="card">
+            <img src={asset.thumbnail6} alt="" />
+            <h2>The tranquil lake shimmered under the golden sunset, while birdsong filled the air, creating a symphony of natural beauty.</h2>
+            <h3>Vikash</h3>
+            <p>15k views &bull; 2 days ago</p>
+        </div>
+        <div className="card">
+            <img src={asset.thumbnail7} alt="" />
+            <h2>The tranquil lake shimmered under the golden sunset, while birdsong filled the air, creating a symphony of natural beauty.</h2>
+            <h3>Vikash</h3>
+            <p>15k views &bull; 2 days ago</p>
+        </div>
+        <div className="card">
+            <img src={asset.thumbnail8} alt="" />
+            <h2>The tranquil lake shimmered under the golden sunset, while birdsong filled the air, creating a symphony of natural beauty.</h2>
             <h3>Vikash</h3>
             <p>15k views &bull; 2 days ago</p>
         </div>
