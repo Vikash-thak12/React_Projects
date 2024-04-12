fddd4d2576b153fd3ed7bd7e2778c96bee061e2a
Vikash Thakur <thakvikash622@gmail.com>
2024-04-12 15:13:29 +0530

    Working on recommended videos list

---
+++ YoutTubeClone/src/Recommend/Recommend.jsx
@@ -0,0 +1,11 @@
+import './Recommend.css'
+
+const Recommend = () => {
+  return (
+    <div className='recommend'>
+        <h1>This is vikash Thakur</h1>
+    </div>
+  );
+};
+
+export default Recommend;
