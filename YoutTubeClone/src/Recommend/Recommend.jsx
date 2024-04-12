10802744f7e37bb3ad15893852f25e7621a223e5
Vikash Thakur <thakvikash622@gmail.com>
2024-04-12 16:17:05 +0530

    Fix the recommend videos section

--- YoutTubeClone/src/Recommend/Recommend.jsx
+++ YoutTubeClone/src/Recommend/Recommend.jsx
@@ -1,10 +1,106 @@
+import { asset } from '../assets/asset';
 import './Recommend.css'
 
 const Recommend = () => {
   return (
     <div className='recommend'>
-        <h1>This is vikash Thakur</h1>
-        <h2>How are you</h2>
+        <div className="side-video-list">
+            <img src={asset.thumbnail4} alt="" />
+            <div className="video-info">
+                <h5>This is vikash thakur biography and here you will get to know about all of my experiences throughout my life
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia ex nobis quasi tempora, praesentium doloribus, necessitatibus voluptatem omnis aliquam minima sunt hic ipsa. Voluptates, odio repellendus deleniti tempora eligendi laboriosam.
+                </h5>
+                <div className="small-info">
+                    <p>Vikash Thakur</p>
+                    <p>2k views &bull; <span>7 days ago</span></p>
+                </div>
+            </div>
+        </div>
+        <div className="side-video-list">
+            <img src={asset.thumbnail1} alt="" />
+            <div className="video-info">
+                <h5>This is vikash thakur biography and here you will get to know about all of my experiences throughout my life
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia ex nobis quasi tempora, praesentium doloribus, necessitatibus voluptatem omnis aliquam minima sunt hic ipsa. Voluptates, odio repellendus deleniti tempora eligendi laboriosam.
+                </h5>
+                <div className="small-info">
+                    <p>Vikash Thakur</p>
+                    <p>2k views &bull; <span>7 days ago</span></p>
+                </div>
+            </div>
+        </div>
+        <div className="side-video-list">
+            <img src={asset.thumbnail2} alt="" />
+            <div className="video-info">
+                <h5>This is vikash thakur biography and here you will get to know about all of my experiences throughout my life
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia ex nobis quasi tempora, praesentium doloribus, necessitatibus voluptatem omnis aliquam minima sunt hic ipsa. Voluptates, odio repellendus deleniti tempora eligendi laboriosam.
+                </h5>
+                <div className="small-info">
+                    <p>Vikash Thakur</p>
+                    <p>2k views &bull; <span>7 days ago</span></p>
+                </div>
+            </div>
+        </div>
+        <div className="side-video-list">
+            <img src={asset.thumbnail3} alt="" />
+            <div className="video-info">
+                <h5>This is vikash thakur biography and here you will get to know about all of my experiences throughout my life
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia ex nobis quasi tempora, praesentium doloribus, necessitatibus voluptatem omnis aliquam minima sunt hic ipsa. Voluptates, odio repellendus deleniti tempora eligendi laboriosam.
+                </h5>
+                <div className="small-info">
+                    <p>Vikash Thakur</p>
+                    <p>2k views &bull; <span>7 days ago</span></p>
+                </div>
+            </div>
+        </div>
+        <div className="side-video-list">
+            <img src={asset.thumbnail5} alt="" />
+            <div className="video-info">
+                <h5>This is vikash thakur biography and here you will get to know about all of my experiences throughout my life
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia ex nobis quasi tempora, praesentium doloribus, necessitatibus voluptatem omnis aliquam minima sunt hic ipsa. Voluptates, odio repellendus deleniti tempora eligendi laboriosam.
+                </h5>
+                <div className="small-info">
+                    <p>Vikash Thakur</p>
+                    <p>2k views &bull; <span>7 days ago</span></p>
+                </div>
+            </div>
+        </div>
+        <div className="side-video-list">
+            <img src={asset.thumbnail6} alt="" />
+            <div className="video-info">
+                <h5>This is vikash thakur biography and here you will get to know about all of my experiences throughout my life
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia ex nobis quasi tempora, praesentium doloribus, necessitatibus voluptatem omnis aliquam minima sunt hic ipsa. Voluptates, odio repellendus deleniti tempora eligendi laboriosam.
+                </h5>
+                <div className="small-info">
+                    <p>Vikash Thakur</p>
+                    <p>2k views &bull; <span>7 days ago</span></p>
+                </div>
+            </div>
+        </div>
+        <div className="side-video-list">
+            <img src={asset.thumbnail7} alt="" />
+            <div className="video-info">
+                <h5>This is vikash thakur biography and here you will get to know about all of my experiences throughout my life
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia ex nobis quasi tempora, praesentium doloribus, necessitatibus voluptatem omnis aliquam minima sunt hic ipsa. Voluptates, odio repellendus deleniti tempora eligendi laboriosam.
+                </h5>
+                <div className="small-info">
+                    <p>Vikash Thakur</p>
+                    <p>2k views &bull; <span>7 days ago</span></p>
+                </div>
+            </div>
+        </div>
+        <div className="side-video-list">
+            <img src={asset.thumbnail8} alt="" />
+            <div className="video-info">
+                <h5>This is vikash thakur biography and here you will get to know about all of my experiences throughout my life
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia ex nobis quasi tempora, praesentium doloribus, necessitatibus voluptatem omnis aliquam minima sunt hic ipsa. Voluptates, odio repellendus deleniti tempora eligendi laboriosam.
+                </h5>
+                <div className="small-info">
+                    <p>Vikash Thakur</p>
+                    <p>2k views &bull; <span>7 days ago</span></p>
+                </div>
+            </div>
+        </div>
+
     </div>
   );
 };
