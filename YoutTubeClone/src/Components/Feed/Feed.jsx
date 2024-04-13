6f948730ece321330109727ed49f5e4c32fe0631
Vikash Thakur <thakvikash622@gmail.com>
2024-04-13 22:28:58 +0530

    Working on the api of youtube category

--- YoutTubeClone/src/Components/Feed/Feed.jsx
+++ YoutTubeClone/src/Components/Feed/Feed.jsx
@@ -1,8 +1,16 @@
 import './Feed.css'
 import { asset } from '../../assets/asset'
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import { API_KEY } from '../../data';
 
-const Feed = () => {
+const Feed = ({category}) => {
+    const [data,setData] = useState(0)
+
+    const fetch_data = async () => {
+        const videolist_url = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=70&regionCode=us&videoCategoryId=${category}&key=${API_KEY}`;
+        await fetch(videolist_url).then(response=> response.json()).then(data=> setData(data.items))
+    }
   return (
     <div className="feed">
         <Link to={`video/20/6834`} className="card">
@@ -11,7 +19,7 @@ const Feed = () => {
             <h3>Vikash</h3>
             <p>15k views &bull; 2 days ago</p>
         </Link>
-        <div className="card">
+        {/* <div className="card">
             <img src={asset.thumbnail2} alt="" />
             <h2>The tranquil lake shimmered under the golden sunset, while birdsong filled the air, creating a symphony of natural beauty.</h2>
             <h3>Vikash</h3>
@@ -100,7 +108,7 @@ const Feed = () => {
             <h2>The tranquil lake shimmered under the golden sunset, while birdsong filled the air, creating a symphony of natural beauty.</h2>
             <h3>Vikash</h3>
             <p>15k views &bull; 2 days ago</p>
-        </div>
+        </div> */}
     </div>
   );
 };
