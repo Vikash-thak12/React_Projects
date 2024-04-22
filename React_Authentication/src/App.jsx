805c1cba75441d8ca0254ccc1d44f113d630b69b
Vikash Thakur <thakvikash622@gmail.com>
2024-04-22 22:27:40 +0530

    Successfully routed the home and profile page under PrivateRoutes

--- React_Authentication/src/App.jsx
+++ React_Authentication/src/App.jsx
@@ -1,6 +1,8 @@
-
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import PrivateRoutes from './utils/PrivateRoutes'
+
+
 import Header from './components/Header'
 import Home from './pages/Home'
 import Profile from './pages/Profile'
@@ -11,13 +13,17 @@ function App() {
 
   return (
     <Router>
-        <Header/>
-        <Routes>
-          <Route path="/login" element={<Login/>}/>
-          <Route path="/register" element={<Register/>}/>
-          <Route path="/" element={<Home/>}/>
-          <Route path="/profile" element={<Profile/>}/>
-        </Routes>
+      <Header />
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+
+        <Route element={<PrivateRoutes/>}>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+        </Route>
+
+      </Routes>
     </Router>
   )
 }
