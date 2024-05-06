66a11d6c265bd7634cad8f4aa3ba6b235600174f
Vikash Thakur <thakvikash622@gmail.com>
2024-05-06 17:40:55 +0530

    Basic structure

--- Dice_Game/Dice_Game/src/App.jsx
+++ Dice_Game/Dice_Game/src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import StartGame from './components/StartGame';
 
 const App = () => {
   return (
     <div>
-    App
+      <StartGame />
     </div>
   );
 };
---
+++ Dice_Game/Dice_Game/src/components/StartGame.jsx
@@ -0,0 +1,18 @@
+
+const StartGame = () => {
+  return (
+    <>
+    <main>
+        <div>
+            <img src="images/dices.png" alt="" />
+        </div>
+        <div className="content">
+            <h1>Dice Game</h1>
+            <button>Play Now</button>
+        </div>
+    </main>
+    </>
+  );
+};
+
+export default StartGame;
