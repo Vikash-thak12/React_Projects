2fa3fee442d6aec30d8f9c313b69bb4e4e55dbe3
Vikash Thakur <thakvikash622@gmail.com>
2024-05-07 14:35:41 +0530

    Final commit

--- Dice_Game/Dice_Game/src/components/StartGame/StartGame.jsx
+++ Dice_Game/Dice_Game/src/components/StartGame/StartGame.jsx
@@ -1,16 +1,18 @@
 import './StartGame.css'
 
-const StartGame = ({toggle}) => {
+const StartGame = ({ toggle }) => {
     return (
-        <main className="flex items-center justify-center min-h-screen">
-            <div>
-                <img src="images/dices.png" alt="" />
-            </div>
-            <div className="content w-auto p-5 rounded">
-                <h1 className="text-8xl">Dice Game</h1>
-                <button onClick={toggle} className="border bg-black text-white py-3 px-8 rounded-md">Play Now</button>
-            </div>
-        </main>
+        <>
+            <main className="flex items-center justify-center min-h-screen">
+                <div>
+                    <img src="images/dices.png" alt="" />
+                </div>
+                <div className="content w-auto p-5 rounded">
+                    <h1 className="text-8xl">Dice Game</h1>
+                    <button onClick={toggle} className="border bg-black text-white py-3 px-8 rounded-md">Play Now</button>
+                </div>
+            </main>
+        </>
     );
 };
 
