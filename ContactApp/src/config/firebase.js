516a3f2d72e824d79a2cb84253b06c52d5a423ea
Vikash Thakur <thakvikash622@gmail.com>
2024-05-09 23:50:27 +0530

    Added firebase credintals

---
+++ ContactApp/src/config/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBOmZVOfiIAiH4FXTpSjvovKNWRD1HBTLI",
+  authDomain: "vite-contact-e28ca.firebaseapp.com",
+  projectId: "vite-contact-e28ca",
+  storageBucket: "vite-contact-e28ca.appspot.com",
+  messagingSenderId: "891966629798",
+  appId: "1:891966629798:web:d40859567c21e6bf385167"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
