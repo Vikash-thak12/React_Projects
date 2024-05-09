c035011ef44dbc126b88e77dd5066c7a42128552
Vikash Thakur <thakvikash622@gmail.com>
2024-05-09 23:28:55 +0530

    Install prettier plugin

--- ContactApp/src/App.jsx
+++ ContactApp/src/App.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
+// import React from 'react';
 
 const App = () => {
   return (
-    <div className='bg-red-800'>
+    <div className='bg-red-800 justify-center flex'>
     App
     </div>
   );
