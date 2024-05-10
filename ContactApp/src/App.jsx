be790df7c2f44558afba7a5daf2a13a105fc6211
Vikash Thakur <thakvikash622@gmail.com>
2024-05-10 17:22:23 +0530

    Fix the modal functionality

--- ContactApp/src/App.jsx
+++ ContactApp/src/App.jsx
@@ -1,8 +1,8 @@
 // import React from 'react';
 import { useState } from "react";
 
-import Modal from "./components/Modal/Modal";
 import Navbar from "./components/Navbar/Navbar";
+import AddandUpdateContact from "./components/AddandUpdateContact/AddandUpdateContact";
 
 const App = () => {
   const [isopen, setIsopen] = useState(false)
@@ -17,10 +17,7 @@ const App = () => {
   return (
     <div className="mx-auto max-w-[370px]">
       <Navbar onopen={onopen} isopen={isopen} />
-      <Modal  isopen={isopen} onclose={onclose} > 
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis, itaque at necessitatibus reprehenderit quis quo deleniti nostrum architecto libero repellendus, assumenda voluptatum velit doloribus est facere inventore. Qui, voluptas maxime?
-      </Modal>
-      
+      <AddandUpdateContact onclose={onclose} isopen={isopen} />
     </div>
   );
 };
---
+++ ContactApp/src/components/AddandUpdateContact/AddandUpdateContact.jsx
@@ -0,0 +1,11 @@
+import Modal from "../Modal/Modal";
+
+const AddandUpdateContact = ({isopen, onclose}) => {
+    return (
+        <Modal isopen={isopen} onclose={onclose} >
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis, itaque at necessitatibus reprehenderit quis quo deleniti nostrum architecto libero repellendus, assumenda voluptatum velit doloribus est facere inventore. Qui, voluptas maxime?
+        </Modal>
+    );
+};
+
+export default AddandUpdateContact;
