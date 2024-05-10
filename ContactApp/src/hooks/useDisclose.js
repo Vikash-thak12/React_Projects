06b102395e509d49ccf41c6433329ec3827e79f1
Vikash Thakur <thakvikash622@gmail.com>
2024-05-10 22:41:24 +0530

    Created a custom hook

---
+++ ContactApp/src/hooks/useDisclose.js
@@ -0,0 +1,17 @@
+import { useState } from "react"
+
+const useDisclose = () => {
+
+    const [isopen, setIsopen] = useState(false)
+
+    const onopen = () => {
+        setIsopen(true)
+    }
+
+    const onclose = () => {
+        setIsopen(false)
+    }
+    return {isopen, onopen, onclose}
+}
+
+export default useDisclose
