90fadc130b5ea16f95d69fbb05f750ee388e15f2
Vikash Thakur <thakvikash622@gmail.com>
2024-05-10 15:53:03 +0530

    Finally fetching of datas are working

--- ContactApp/src/components/Navbar/Navbar.jsx
+++ ContactApp/src/components/Navbar/Navbar.jsx
@@ -1,34 +1,35 @@
 import { FiSearch } from 'react-icons/fi';
 import './Navbar.css'
 // import { AiFillPlayCircle } from 'react-icons/ai';
-import { FaPlusCircle } from 'react-icons/fa';
+import { FaPlusCircle, FaRegUserCircle } from 'react-icons/fa';
 import { useEffect, useState } from 'react';
 import { collection, getDocs } from 'firebase/firestore';
 import { db } from '../../config/firebase';
+import { RiEditCircleLine } from 'react-icons/ri';
+import { IoMdTrash } from 'react-icons/io';
 
 const Navbar = () => {
 
     const [contacts, setContacts] = useState([])
 
-    useEffect( () => {
+    useEffect(() => {
         const getcontacts = async () => {
             try {
                 const contactsRef = collection(db, "contacts");
                 const contactsnapShot = await getDocs(contactsRef);
-                const contactlists = contactsnapShot.docs.map( (doc) => {
+                const contactlists = contactsnapShot.docs.map((doc) => {
                     return {
                         id: doc.id,
                         ...doc.data(),
                     }
                 });
-                console.log(contactlists);
-                // console.log(contactsnapShot);
+                setContacts(contactlists);
             } catch (error) {
                 console.log(error)
             }
         }
         getcontacts();
-    },[])
+    }, [])
     return (
         <>
             <div className="my-4 p-5 rounded-lg bg-white">
@@ -40,12 +41,31 @@ const Navbar = () => {
             <div className='flex gap-2'>
                 <div className='flex items-center justify-center w-full relative'>
                     <FiSearch className=' text-white text-2xl absolute left-2' />
-                    <input type="text" placeholder='Search' 
-                    className='flex-grow border border-white bg-transparent rounded-md px-12 py-3' />
+                    <input type="text" placeholder='Search'
+                        className='flex-grow border border-white bg-transparent rounded-md px-12 py-3' />
                 </div>
                 <FaPlusCircle className='text-white text-5xl' />
                 <div>
                 </div>
+            </div>
+            <div className="datas mt-5 py-2 rounded-md gap-3 flex flex-col">
+                {
+                    contacts.map((contact) => (
+                        <div key={contact.id} className='data'>
+                            <div className='flex items-center gap-2'>
+                                <FaRegUserCircle className='text-4xl text-orange cursor-pointer' />
+                                <div className=''>
+                                    <h2 className='font-bold'>{contact.name}</h2>
+                                    <p>{contact.Email}</p>
+                                </div>
+                            </div>
+                            <div className="icons flex text-4xl text-blue-700 cursor-pointer">
+                                <RiEditCircleLine />
+                                <IoMdTrash />
+                            </div>
+                        </div>
+                    ))
+                }
             </div>
         </>
     );
