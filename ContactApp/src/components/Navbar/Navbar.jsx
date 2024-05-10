04739a45ccdf45b2bd5dff32de446a3575b3fa8e
Vikash Thakur <thakvikash622@gmail.com>
2024-05-10 15:21:25 +0530

    Working on the functionality to fetch the data from the server

--- ContactApp/src/components/Navbar/Navbar.jsx
+++ ContactApp/src/components/Navbar/Navbar.jsx
@@ -1,6 +1,34 @@
+import { FiSearch } from 'react-icons/fi';
 import './Navbar.css'
+// import { AiFillPlayCircle } from 'react-icons/ai';
+import { FaPlusCircle } from 'react-icons/fa';
+import { useEffect, useState } from 'react';
+import { collection, getDocs } from 'firebase/firestore';
+import { db } from '../../config/firebase';
 
 const Navbar = () => {
+
+    const [contacts, setContacts] = useState([])
+
+    useEffect( () => {
+        const getcontacts = async () => {
+            try {
+                const contactsRef = collection(db, "contacts");
+                const contactsnapShot = await getDocs(contactsRef);
+                const contactlists = contactsnapShot.docs.map( (doc) => {
+                    return {
+                        id: doc.id,
+                        ...doc.data(),
+                    }
+                });
+                console.log(contactlists);
+                // console.log(contactsnapShot);
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        getcontacts();
+    },[])
     return (
         <>
             <div className="my-4 p-5 rounded-lg bg-white">
@@ -9,9 +37,16 @@ const Navbar = () => {
                     <h1 className='font-bold text-2xl'>Firebase Contact App</h1>
                 </div>
             </div>
-            <div className='flex items-center justify-center w-full'>
-                    <input type="text" placeholder='Search' className='flex-grow border border-white bg-transparent h-10 rounded-md' />
+            <div className='flex gap-2'>
+                <div className='flex items-center justify-center w-full relative'>
+                    <FiSearch className=' text-white text-2xl absolute left-2' />
+                    <input type="text" placeholder='Search' 
+                    className='flex-grow border border-white bg-transparent rounded-md px-12 py-3' />
                 </div>
+                <FaPlusCircle className='text-white text-5xl' />
+                <div>
+                </div>
+            </div>
         </>
     );
 };
